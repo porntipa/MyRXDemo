0f3df97e4e77b8db2a09769a53d82b719fdd02f5
Porntipa Choksungnoen <porntipa.chok@gmail.com>
2018-04-09 13:44:07 +0700

    add Method ให้ layout ที่  copy มา

--- app/src/main/java/com/example/rezzipuy/myrxdemo/MainActivity.java
+++ app/src/main/java/com/example/rezzipuy/myrxdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rezzipuy.myrxdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickZip(View v){
+
+    }
+
+    public void onClickConcat(View v){
+
+    }
+
+    public void onClickMerge(View v){
+
     }
 }
