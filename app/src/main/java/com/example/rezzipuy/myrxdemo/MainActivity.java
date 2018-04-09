aba6dffbd27fdc6fa8001bcf6f3ef3f4da41d5ab
Porntipa Choksungnoen <porntipa.chok@gmail.com>
2018-04-09 13:47:48 +0700

    copy ตัวแปร Observable

--- app/src/main/java/com/example/rezzipuy/myrxdemo/MainActivity.java
+++ app/src/main/java/com/example/rezzipuy/myrxdemo/MainActivity.java
@@ -3,8 +3,18 @@ package com.example.rezzipuy.myrxdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Observable<String> jsonFeedObservable;
+    private Observable<String> xmlFeedObservable;
+    private TextView mTextview;
+    private TextView mDescTextView;
+    private Disposable subscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
