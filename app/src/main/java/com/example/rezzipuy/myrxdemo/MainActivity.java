feea1d4b5e712528fd735b2324358dbbcf85747f
Porntipa Choksungnoen <porntipa.chok@gmail.com>
2018-04-09 13:58:24 +0700

    general feeding code with okHttp

--- app/src/main/java/com/example/rezzipuy/myrxdemo/MainActivity.java
+++ app/src/main/java/com/example/rezzipuy/myrxdemo/MainActivity.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.IOException;
+
 import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextview = (TextView) findViewById(R.id.textview);
+        mDescTextView = (TextView) findViewById(R.id.descTextView);
     }
 
     public void onClickZip(View v){
@@ -33,4 +40,18 @@ public class MainActivity extends AppCompatActivity {
     public void onClickMerge(View v){
 
     }
+
+    // general feeding code with okHttp
+    public String feedDataFromUrl(final String url) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        String result = response.body().string();
+
+        return result;
+    }
+
 }
