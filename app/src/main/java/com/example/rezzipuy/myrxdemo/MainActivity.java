337203cc0954ae65ec2204721a4d8b6bdc15468e
Porntipa Choksungnoen <porntipa.chok@gmail.com>
2018-04-09 14:10:04 +0700

    create JSON & xml Observable

--- app/src/main/java/com/example/rezzipuy/myrxdemo/MainActivity.java
+++ app/src/main/java/com/example/rezzipuy/myrxdemo/MainActivity.java
@@ -8,7 +8,11 @@ import android.widget.TextView;
 import java.io.IOException;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Scheduler;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -27,6 +31,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mTextview = (TextView) findViewById(R.id.textview);
         mDescTextView = (TextView) findViewById(R.id.descTextView);
+
+        //JSON Observable
+        jsonFeedObservable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                String result = feedDataFromUrl("http://codemobiles.com/adhoc/feed/youtube_feed.php?type=json");            }
+        });
+
+        // xml Observable
+        xmlFeedObservable =Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+
+
+            }
+        });
+
     }
 
     public void onClickZip(View v){
